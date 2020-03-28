ddf954cc75778af784f602159f17f9c53b375560
WerHawer <werhawer@gmail.com>
2020-03-28 21:11:50 +0200

    Updates

---
+++ precache-manifest.15bf092f7584b4458019de6f0bbb6ae7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5122846a294aa8f48b56a3d06b9b33f9",
+    "url": "/maze/index.html"
+  },
+  {
+    "revision": "4bb6c263fd8afc24f204",
+    "url": "/maze/static/css/main.7f178138.chunk.css"
+  },
+  {
+    "revision": "6c61ce6462e804983476",
+    "url": "/maze/static/js/2.222b066a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maze/static/js/2.222b066a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb6c263fd8afc24f204",
+    "url": "/maze/static/js/main.fc15ae6b.chunk.js"
+  },
+  {
+    "revision": "8db2ea21281f1c0f6025",
+    "url": "/maze/static/js/runtime-main.8b78a93f.js"
+  },
+  {
+    "revision": "d37889dd35065eae5b78ed3522a69a95",
+    "url": "/maze/static/media/lose.d37889dd.png"
+  },
+  {
+    "revision": "7ff152ba06292cb37defa35d31d684f0",
+    "url": "/maze/static/media/start.7ff152ba.svg"
+  },
+  {
+    "revision": "8e93251ce70f1a27fd7de6e6ae2fefc1",
+    "url": "/maze/static/media/win.8e93251c.png"
+  }
+]);
